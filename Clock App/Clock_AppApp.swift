f8f1045e8f2b1ea2983d73239a0d8aaaef76fdec
Clement Ojiguo <clementojigs@hotmail.com>
2023-08-29 12:14:08 +0100

    Initial Commit

---
+++ Clock App/Clock_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clock_AppApp.swift
+//  Clock App
+//
+//  Created by MACBOOK PRO 13 on 29/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct Clock_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
